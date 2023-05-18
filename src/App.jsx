fabac4f35bceded747adf2fe46cc877a7e68c4ed
Bilen Berhane <bberhane.3@gmail.com>
2023-05-18 19:28:59 -0400

    formatting 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import placeArray from './places.json'
 const App = () => {
   return (
     <div>
-      <h1 className="title">Bucket List : Travel Destinations for 2025</h1>
+      <h1 className="title">Bucket List : Travel Destinations</h1>
       <main>
         {placeArray.map((place) => (
           <Country country={place} key={place.capital} />
